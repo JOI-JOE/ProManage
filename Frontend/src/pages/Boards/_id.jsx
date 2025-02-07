17e0cbc83f92492aa68e62f38561df8c66d910b9
phamthihongngat <hongngat161102@hmail.com>
2025-02-07 10:29:15 +0700

    cm1

---
+++ Frontend/src/pages/Boards/_id.jsx
@@ -0,0 +1,25 @@
+// import React from "react";
+
+import { Box, Container } from "@mui/material";
+import BoardBar from "./BoardBar/BoardBar";
+import BoardContent from "./BoardConten/BoardContent";
+import AppBar from "~/components/AppBar/AppBar";
+import SideBar from "./SideBar/SideBar";
+import { mockData } from "~/Apis/mock-data";
+
+const Board = () => {
+  return (
+    <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
+      <AppBar />
+      <Box sx={{ display: "flex" }}>
+        <SideBar />
+        <Box sx={{ width: "81%" }}>
+          <BoardBar board={mockData?.board} />
+          <BoardContent board={mockData?.board} />
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default Board;
