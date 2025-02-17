b3d756e00daafb0a4479607f813962da291d5171
phamthihongngat <hongngat161102@hmail.com>
2025-02-17 21:24:40 +0700

    Add new column

--- Frontend/src/pages/Boards/BoardConten/ListColumns/ListColumns.jsx
+++ Frontend/src/pages/Boards/BoardConten/ListColumns/ListColumns.jsx
@@ -1,12 +1,20 @@
-import { Box, Button } from "@mui/material";
+import { Box, Button, InputAdornment, TextField } from "@mui/material";
 import Column from "./Column/Column";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import CloseIcon from "@mui/icons-material/Close";
 import {
   SortableContext,
   horizontalListSortingStrategy,
 } from "@dnd-kit/sortable";
+import { useState } from "react";
 
 const ListColumns = ({ columns }) => {
+  const [openColumn, setOpenColumn] = useState(false); // State để kiểm soát hiển thị input
+  const toggleOpenColumn = () => setOpenColumn(!openColumn);
+  const [columnName, setColumnName] = useState("");
+  const addColumn = () => {
+    console.log("columnName");
+  };
   return (
     <SortableContext
       items={columns?.map((c) => c._id)}
@@ -31,29 +39,90 @@ const ListColumns = ({ columns }) => {
         ))}
 
         {/* Box Add Column */}
-        <Box
-          sx={{
-            minWidth: "200px",
-            maxWidth: "200px",
-            mx: 2,
-            borderRadius: "6px",
-            height: "fit-content",
-            bgcolor: "#ffffff3d",
-          }}
-        >
-          <Button
-            startIcon={<NoteAddIcon />}
+        {!openColumn ? (
+          <Box
+            onClick={toggleOpenColumn}
             sx={{
-              color: "#ffffff",
-              width: "100%",
-              justifyContent: "flex-start",
-              pl: 2.5,
-              py: 1,
+              minWidth: "250px",
+              maxWidth: "250px",
+              mx: 2,
+              borderRadius: "6px",
+              height: "fit-content",
+              bgcolor: "#ffffff3d",
             }}
           >
-            Add new column
-          </Button>
-        </Box>
+            <Button
+              startIcon={<NoteAddIcon />}
+              sx={{
+                color: "#ffffff",
+                width: "100%",
+                justifyContent: "flex-start",
+                pl: 2.5,
+                py: 1,
+              }}
+            >
+              Add new column
+            </Button>
+          </Box>
+        ) : (
+          <Box
+            sx={{
+              minWidth: "250px",
+              maxWidth: "250px",
+              mx: 2,
+              p: 1,
+              borderRadius: "6px",
+              height: "fit-content",
+              bgcolor: "#ffffff3d",
+              display: "flex",
+              flexDirection: "column",
+              gap: 1,
+            }}
+          >
+            <TextField
+              label="Enter..."
+              type="text"
+              size="small"
+              variant="outlined"
+              autoFocus
+              value={columnName}
+              onChange={(e) => setColumnName(e.target.value)}
+              sx={{
+                "& label": { color: "white" },
+                "& input": { color: "white", fontSize: "14px" },
+                "& label.Mui-focused": { color: "white" },
+                "& .MuiOutlinedInput-root": {
+                  "& fieldset": { borderColor: "white" },
+                  "&:hover fiedldset": { borderColor: "white" },
+                  "&.Mui-focused fieldset": { borderColor: "white" },
+                },
+              }}
+            />
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <Button
+                onClick={addColumn}
+                variant="contained"
+                color="success"
+                size="small"
+                sx={{
+                  boxShadow: "none",
+                  border: "none",
+                  bgcolor: "teal",
+                }}
+              >
+                Add Column
+              </Button>
+              <CloseIcon
+                fontSize="small"
+                sx={{
+                  color: "white",
+                  cursor: "pointer",
+                }}
+                onClick={toggleOpenColumn}
+              />
+            </Box>
+          </Box>
+        )}
       </Box>
     </SortableContext>
   );
