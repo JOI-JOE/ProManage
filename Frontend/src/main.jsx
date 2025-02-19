25c9b61efd6530af7c9104a94f51154ce7a4320f
JOI-JOE <iknowhtml161gmail.com>
2025-02-19 17:50:13 +0700

    Fix file main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -9,6 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 // import { RouterProvider } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { ContextProvider } from "./contexts/ContextProvider";
 import theme from "~/theme";
