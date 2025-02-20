a8f02579e5a0d561c91c2b55df14cd8755cd31f5
JOI-JOE <iknowhtml161gmail.com>
2025-02-20 23:43:52 +0700

    Fix file index.jsx

--- Frontend/src/components/Navigation/WorkspaceMenu/index.jsx
+++ Frontend/src/components/Navigation/WorkspaceMenu/index.jsx
@@ -11,104 +11,114 @@ import SettingsIcon from '@mui/icons-material/Settings'
 import AddIcon from '@mui/icons-material/Add'
 import { Link } from 'react-router-dom'
 
-import ViewKanbanIcon from "@mui/icons-material/ViewKanban";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
-import ViewStreamIcon from "@mui/icons-material/ViewStream";
-import PeopleIcon from "@mui/icons-material/People";
-import SettingsIcon from "@mui/icons-material/Settings";
-import AddIcon from "@mui/icons-material/Add";
-import { Link } from "react-router-dom";
-import Authen from "../../../../Apis/Authen";
-
-function WorkspaceMenu() {
-  const [openSettings, setOpenSettings] = useState({}); // Lưu trạng thái mở của từng workspace
-  const [hoveredItem, setHoveredItem] = useState(null);
-  const [workspaces, setWorkspaces] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // Toggle trạng thái mở của workspace
-  const toggleSettings = (workspaceId) => {
-    setOpenSettings((prev) => ({
-      ...prev,
-      [workspaceId]: !prev[workspaceId],
-    }));
-  };
-
-  useEffect(() => {
-    const fetchWorkspaces = async () => {
-      try {
-        const response = await Authen.get("/workspaces");
-        setWorkspaces(response.data.data);
-        setLoading(false);
-      } catch (err) {
-        setError("Có lỗi khi lấy dữ liệu.");
-        setLoading(false);
-      }
-    };
-
-    fetchWorkspaces();
-  }, []);
 
 
-    return (
-        <div id="workspace-main">
-            <ListItemButton key="workspace-item" onClick={toggleSettings} sx={{ display: "flex", justifyContent: "space-between" }}>
-                <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
-                    <ListItemIcon sx={{ color: "black" }}>
-                        <Avatar sx={{ bgcolor: "#5D87FF" }}>
-                            L {/* Hiển thị chữ cái đầu của tên workspace */}
-                        </Avatar>
-                    </ListItemIcon>
-                    <ListItemText
-                        primary={
-                            <Typography fontWeight="bold" sx={{ whiteSpace: "nowrap", color: "black" }}>
-                                {workspace.name}
-                            </Typography>
-                        }
-                    />
-                </Box>
+const WorkspaceMenu = ({ workspace }) => {
+  const [openSettings, setOpenSettings] = useState(false)
+  const [hoveredItem, setHoveredItem] = useState(null)
+  const [workspaces, setWorkspaces] = useState([]); // State để lưu workspace
+  const [loading, setLoading] = useState(true); // State để kiểm tra trạng thái loading
+  const [error, setError] = useState(null); // State để lưu lỗi (nếu có)
 
-                {openSettings ? (
-                    <ExpandLess sx={{ color: "black" }} />
-                ) : (
-                    <ExpandMore sx={{ color: "black" }} />
-                )}
+
+  const toggleSettings = ({ workspace }) => {
+    setOpenSettings(!openSettings)
+  }
+
+  const handleMouseEnter = (item) => {
+    setHoveredItem(item)
+  }
+
+  const handleMouseLeave = () => {
+    setHoveredItem(null)
+  }
+
+
+  return (
+    <div id="workspace-main">
+      <ListItemButton key="workspace-item" onClick={toggleSettings} sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ display: "flex", alignItems: "center", gap: "10px" }}>
+          <ListItemIcon sx={{ color: "black" }}>
+            <Avatar sx={{ bgcolor: "#5D87FF" }}>
+              L {/* Hiển thị chữ cái đầu của tên workspace */}
+            </Avatar>
+          </ListItemIcon>
+          <ListItemText
+            primary={
+              <Typography fontWeight="bold" sx={{ whiteSpace: "nowrap", color: "black" }}>
+                {workspace.name}
+              </Typography>
+            }
+          />
+        </Box>
+
+        {openSettings ? (
+          <ExpandLess sx={{ color: "black" }} />
+        ) : (
+          <ExpandMore sx={{ color: "black" }} />
+        )}
+      </ListItemButton>
+
+      <Collapse in={openSettings} timeout="auto" unmountOnExit>
+        <Box sx={{ pl: 4 }}>
+          <List>
+            <ListItemButton component={Link} to={`/w/${workspace.display_name}/home`}>
+              <ListItemIcon sx={{ color: "black" }}>
+                <ViewKanbanIcon />
+              </ListItemIcon>
+              <ListItemText primary="Bảng" sx={{ color: "black" }} />
+            </ListItemButton>
+            <ListItemButton>
+              <ListItemIcon sx={{ color: "black" }}>
+                <FavoriteBorderIcon />
+              </ListItemIcon>
+              <ListItemText primary="Điểm nổi bật" sx={{ color: "black" }} />
             </ListItemButton>
 
-            <Collapse in={openSettings} timeout="auto" unmountOnExit>
-                <Box sx={{ pl: 4 }}>
-                    <List>
-                        <ListItemButton component={Link} to={`/w/${workspace.display_name}/home`}>
-                            <ListItemIcon sx={{ color: "black" }}>
-                                <ViewKanbanIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Bảng" sx={{ color: "black" }} />
-                        </ListItemButton>
-                        <ListItemButton>
-                            <ListItemIcon sx={{ color: "black" }}>
-                                <FavoriteBorderIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Điểm nổi bật" sx={{ color: "black" }} />
-                        </ListItemButton>
+            <ListItemButton
+              onMouseEnter={() => handleMouseEnter("Hình")}
+              onMouseLeave={handleMouseLeave}
+            >
+              <ListItemIcon sx={{ color: "black" }}>
+                <ViewStreamIcon />
+              </ListItemIcon>
+              <ListItemText primary="Hình" sx={{ color: "black" }} />
+              {hoveredItem === "Hình" && (
+                <ChevronRightIcon sx={{ color: "gray" }} />
+              )}
+            </ListItemButton>
 
-                <ListItemButton
-                  onMouseEnter={() => setHoveredItem("Cài đặt")}
-                  onMouseLeave={() => setHoveredItem(null)}
-                >
-                  <ListItemIcon sx={{ color: "black" }}>
-                    <SettingsIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Cài đặt" sx={{ color: "black" }} />
-                  {hoveredItem === "Cài đặt" && <ChevronRightIcon sx={{ color: "gray" }} />}
-                </ListItemButton>
-              </List>
-            </Box>
-          </Collapse>
-        </div>
-      ))}
+            <ListItemButton
+              onMouseEnter={() => handleMouseEnter("Thành viên")}
+              onMouseLeave={handleMouseLeave}
+            >
+              <ListItemIcon sx={{ color: "black" }}>
+                <PeopleIcon />
+              </ListItemIcon>
+              <ListItemText primary="Thành viên" sx={{ color: "black" }} />
+              <AddIcon sx={{ color: "gray" }} />
+              {hoveredItem === "Thành viên" && (
+                <ChevronRightIcon sx={{ color: "gray" }} />
+              )}
+            </ListItemButton>
+
+            <ListItemButton
+              onMouseEnter={() => handleMouseEnter("Cài đặt")}
+              onMouseLeave={handleMouseLeave}
+            >
+              <ListItemIcon sx={{ color: "black" }}>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Cài đặt" sx={{ color: "black" }} />
+              {hoveredItem === "Cài đặt" && (
+                <ChevronRightIcon sx={{ color: "gray" }} />
+              )}
+            </ListItemButton>
+          </List>
+        </Box>
+      </Collapse>
     </div>
-  );
+  )
 }
 
-export default WorkspaceMenu;
+export default WorkspaceMenu
