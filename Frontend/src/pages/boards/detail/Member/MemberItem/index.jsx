9a2de10e32a7dc0042088f84dc73a6cf70cf2c9e
phanquoc123 <phanquoc1512@gmail.com>
2025-04-03 21:48:20 +0700

    update UI

--- Frontend/src/pages/boards/detail/Member/MemberItem/index.jsx
+++ Frontend/src/pages/boards/detail/Member/MemberItem/index.jsx
@@ -42,9 +42,10 @@ const MemberItem = ({ member }) => {
         alignItems: "center",
         justifyContent: "space-between",
         padding: "10px",
-        borderBottom: "1px solid #333",
-        background: "#222",
-        borderRadius: "8px",
+        // borderBottom: "1px solid #333",
+        // borderTop: "1px solid #333",
+        background: "#ffffff",
+        // borderRadius: "8px",
         marginBottom: "8px",
       }}
     >
@@ -62,7 +63,7 @@ const MemberItem = ({ member }) => {
           {member?.name?.charAt(0)}
         </Avatar>
         <Box>
-          <Typography fontWeight="bold" sx={{ color: "#fff" }}>
+          <Typography fontWeight="bold" sx={{ color: "#172B4D" }}>
             {member?.name}
           </Typography>
           <Typography variant="body2" sx={{ color: "gray" }}>
@@ -112,7 +113,7 @@ const MemberItem = ({ member }) => {
           size="small"
           startIcon={<HelpOutlineIcon />}
         >
-          {member.member_type}
+          {member.member_type === "admin" ? "Quản trị viên" : "Thành viên"}
         </Button>
 
         <Button
